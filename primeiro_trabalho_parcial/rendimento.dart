1bf04dbf8cd9f855b51314d1df490c9420c85e52
sergio-frazon <161370389+sergio-frazon@users.noreply.github.com>
2026-03-27 12:28:28 +0000

    rendimento mensal

---
+++ primeiro_trabalho_parcial/rendimento.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(){
+  stdout.write('Qual o valor do depósito desejado?');
+  
+  double? deposito = double.tryParse(stdin.readLineSync()!) ?? 0.0;
+
+  double rendimento = deposito * 1.005;
+  stdout.write('Valor do rendimento após um mês: $rendimento \n');
+
+  
+}
