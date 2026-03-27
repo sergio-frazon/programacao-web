ea4969d007559e8324d26c3d2d97a7ea5f584bd5
sergio-frazon <161370389+sergio-frazon@users.noreply.github.com>
2026-03-27 12:30:19 +0000

    metros para centimetros

---
+++ primeiro_trabalho_parcial/metroscentimetros.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+void main(){
+
+  stdout.write('Digite o valor em metros:');
+  double metros = double.parse(stdin.readLineSync()!);
+
+  double centimetros = metros * 100;
+
+stdout.write('\n Resultado:');
+stdout.write('$metros metros equivalem a $centimetros centímetros \n');
+}
